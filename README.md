35d90c0d742e32518d74280b856e105667f03390
Karlos Gomez <fenyr.sh@gmail.com>
2019-03-06 02:49:27 -0600

    Installation process finished.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Firefox installer for unsupported systems
 
+This script allows you to install the latest version of firefox for
+operating systems that do not have updated repositories.
--- install.sh
+++ install.sh
@@ -2,18 +2,20 @@
 
 # https://ftp.mozilla.org/pub/firefox/releases/latest/README.txt
 
-# if [ $(id -u) != "0" ]; then
-#     echo "Please run this script as root"
-#     exit 1
-# fi
+if [ $(id -u) != "0" ]; then
+    echo -e "\e[1;31mPlease run this script as root!\e[0m"
+    exit 1
+fi
 
+cache=/home/$SUDO_USER/.cache/mozilla
+config=/home/$SUDO_USER/.mozilla
 prefix=/opt
 arch='linux64'
 lang='en-US'
 filename='firefox.tar.bz2'
 url="https://download.mozilla.org/?product=firefox-latest&os=$arch&lang=$lang"
 
-# Find machine architecture.
+# Check the machine architecture.
 case $(arch) in
     x86_64)
         arch='linux64'
@@ -25,9 +27,6 @@ esac
 
 function desktop() {
     {
-        echo "#***************************#"
-        echo "#    Created by fenyr-sh    #"
-        echo "#***************************#"
         echo "[Desktop Entry]"
         echo "Name=Firefox"
         echo "Comment=Firefox Browser"
@@ -41,28 +40,71 @@ function desktop() {
 }
 
 function download() {
-    wget -O $filename $url
+    # If the $filename file does not exist, then it is downloaded.
+    if [[ ! (-e $filename) ]]; then
+        wget --no-verbose --show-progress -O $filename $url # --content-disposition
+    else
+        echo -e "\e[1;34mThe file already exists!\e[0m"
+    fi
 }
 
-function unpack() {
-    echo -e "\e[1;33mUnpacking $filename...\e[0m"
+function extract() {
+    # Remove firefox installation folder if exists.
+    if [[ -d "$prefix/firefox" ]]; then
+        echo -e "\e[1;33mRemoving $prefix/firefox...\e[0m"
+        rm -r $prefix/firefox
+        echo -e "\e[1;32m$prefix/firefox removed!\e[0m"
+    fi
+
+    # Extract $filename in $prefix
+    echo -e "\e[1;33mExtracting $filename...\e[0m"
     tar jxvf $filename -C $prefix &> /dev/null
-    echo -e "\e[1;32m$filename unpacked!\e[0m"
+
+    if [[ $? -ne 0 ]]; then
+        echo -e "\e[1;32mSomething went wrong when extracting the file!\e[0m"
+    else
+        echo -e "\e[1;32m$filename extracted!\e[0m"
+    fi
 }
 
 function install() {
     download
 
     if [[ $? -eq 0 ]]; then
-        unpack
+        extract
 
         if [[ $? -eq 0 ]]; then
-
             desktop
 
-            echo -e "\e[1;32mFirefox installed!\e[0m"
+            if [[ $? -ne 0 ]]; then
+                echo -e "\e[1;32mSomething went wrong when trying to create the desktop file!\e[0m"
+            else
+                echo -e "\e[1;32mDesktop file created successfully!\e[0m"
+            fi
         fi
     fi
 }
 
+function clean_cache() {
+    # Clean cache and configuration
+    if [[ -d $cache ]]; then
+        echo -e "\e[1;33mCleaning $cache...\e[0m"
+        rm -r $cache
+    fi
+    
+    if [[ -d $config ]]; then
+        echo -e "\e[1;33mCleaning $config...\e[0m"
+        rm -r $config
+    fi
+    
+    echo -e "\e[1;32mCleaned successfully!\e[0m"
+}
+
+function clean() {
+    rm $filename
+}
+
 install
+# clean_cache
+clean
+echo -e "\e[1;32mFirefox installed!\e[0m"
